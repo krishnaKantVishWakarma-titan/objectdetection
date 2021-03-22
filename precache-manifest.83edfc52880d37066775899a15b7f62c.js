5a62489482996ebdb2efdef322934264006fa19a
Krishna kant vishwakarma <krishnavishwakarma80919@gmail.com>
2021-03-22 13:50:27 +0530

    first

---
+++ precache-manifest.83edfc52880d37066775899a15b7f62c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40016034b7351af799ca78b0602ba5cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8815190b69453c3b122",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3937a876c0dfc00d7d5a",
+    "url": "/static/js/2.f2d03e77.chunk.js"
+  },
+  {
+    "revision": "7dc2529ea9c355027961799bc1ffe0c3",
+    "url": "/static/js/2.f2d03e77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8815190b69453c3b122",
+    "url": "/static/js/main.a9998311.chunk.js"
+  },
+  {
+    "revision": "949eb168f1d833ae83d5",
+    "url": "/static/js/runtime-main.743d20cc.js"
+  }
+]);
